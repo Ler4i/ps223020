74bea251f2bc0451862454d93145854794934728
Sashagusew <rzhuludkov@bk.ru>
2022-04-10 17:49:54 +0300

    di

---
+++ ps223020/BusinessLogic.Core/DI.cs
@@ -0,0 +1,17 @@
+﻿using ps223020.DbContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ps223020.BusinessLogic.Core
+{
+    public class DI//инъекция зависимости 
+    {
+        private readonly Context _context;
+        public DI(Context context)
+        {
+            _context = context;
+        }
+    }
+}
